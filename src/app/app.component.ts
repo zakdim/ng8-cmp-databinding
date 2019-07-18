2be89bc0f8c5cfe904d96b8017efad6ca8ccaca0
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-07-18 00:38:47 -0400

    s05-67 binding to custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8-cmp-databinding';
+  serverElements = [
+    {type: 'server', name: 'TestServer', content: 'Just a test!'}];
+
+    onServerAdded(serverData: {serverName: string, serverContent: string}) {
+      this.serverElements.push({
+        type: 'server',
+        name: serverData.serverName,
+        content: serverData.serverContent
+      });
+    }
+  
+    onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+      this.serverElements.push({
+        type: 'blueprint',
+        name: blueprintData.serverName,
+        content: blueprintData.serverContent
+      });
+    }
+
 }
